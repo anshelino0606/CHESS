ea1639b42a6064755194edc1593f9a36e8b6bcdd
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-19 12:59:49 +0300

    added template for FEN notation

--- ENGINE/Board.cpp
+++ ENGINE/Board.cpp
@@ -3,3 +3,125 @@
 //
 
 #include "Board.h"
+#include "./GRAPHICS/resourceManager.h"
+
+void Board::init() {
+
+    ResourceManager::loadTexture("", true, "light");
+    ResourceManager::loadTexture("", true, "dark");
+
+    // TODO: load as a game level
+
+    for (int i = 0; i < 32; i++) {
+        // TODO: implement pawn initialization
+        // PARSE FEN NOTATION
+    }
+}
+
+
+
+Board::Board() {
+    board = new char[NB_SQ];
+    castleRights = new bool*[NB_COLOR];
+    for (int i = 0; i < NB_COLOR; ++i) {
+        castleRights[i] = new bool[NB_CASTLE];
+    }
+
+
+}
+
+Board::~Board() {
+    delete[] board;
+    for (int i = 0; i < NB_COLOR; i++) {
+        delete[] castleRights[i];
+    }
+    delete[] castleRights;
+}
+
+void Board::parseFen(const std::string& FEN)
+{
+    size_t iter = 0;
+    int index = 0;
+
+    // Function to set the squares on the board
+    auto set_squares = [&](char piece, int count) {
+        for (int i = 0; i < count; ++i)
+        {
+            if (index < NB_SQ)
+            {
+                board[index] = piece;
+                ++index;
+            }
+            else
+            {
+                // Handle malformed FEN (buffer overflow)
+                // You may want to throw an exception or handle this differently
+                break;
+            }
+        }
+    };
+
+    // Parse the board
+    while (iter < FEN.size() && FEN[iter] != ' ')
+    {
+        if (FEN[iter] == '/')
+        {
+            // Handle malformed FEN (no more than 8 files in a rank)
+            // You may want to throw an exception or handle this differently
+        }
+        else if (isdigit(FEN[iter]))
+        {
+            int count = FEN[iter] - '0';
+            set_squares('.', count);
+        }
+        else
+        {
+            set_squares(FEN[iter], 1);
+        }
+        ++iter;
+    }
+
+    // Parse the turn
+    ++iter;
+    if (iter < FEN.size() && FEN[iter] == 'w')
+    {
+        turn = Color::white;
+    }
+    else if (iter < FEN.size() && FEN[iter] == 'b')
+    {
+        turn = Color::black;
+    }
+
+    // Parse the castling rights
+    iter += 2;
+    while (iter < FEN.size() && FEN[iter] != ' ')
+    {
+        if (FEN[iter] == 'k')
+        {
+            castleRights[static_cast<int>(Color::black)][static_cast<int>(Castle::kingSide)] = true;
+        }
+        else if (FEN[iter] == 'K')
+        {
+            castleRights[static_cast<int>(Color::white)][static_cast<int>(Castle::kingSide)] = true;
+        }
+        else if (FEN[iter] == 'q')
+        {
+            castleRights[static_cast<int>(Color::black)][static_cast<int>(Castle::queenSide)] = true;
+        }
+        else if (FEN[iter] == 'Q')
+        {
+            castleRights[static_cast<int>(Color::white)][static_cast<int>(Castle::queenSide)] = true;
+        }
+        ++iter;
+    }
+
+    // Parse en passant (if available)
+    if (iter + 1 < FEN.size() && FEN[iter] == ' ')
+    {
+        enPassant = FEN.substr(iter + 1, 2);
+    }
+    else
+    {
+        enPassant = ""; // No en passant available
+    }
+}
--- ENGINE/Board.h
+++ ENGINE/Board.h
@@ -5,8 +5,48 @@
 #ifndef CHESS_BOARD_H
 #define CHESS_BOARD_H
 
+#include <string>
+#include "Piece.h"
+#include "./GRAPHICS/texture.h"
+
+constexpr int NB_SQ = 64;       // Number of squares on the chessboard
+constexpr int NB_CASTLE = 2;    // Number of castle types
+constexpr int NB_COLOR = 2;     // Number of colors in chess (white and black)
+
+enum class Castle
+{
+    kingSide,
+    queenSide
+};
+
+enum class Color
+{
+    white,
+    black
+};
 
 class Board {
+public:
+    Board();
+
+    void parseFen(const std::string& FEN);
+    void init();
+    void update();
+    void render();
+
+
+    ~Board();
+
+private:
+    Texture2D lightSquare;
+    Texture2D darkSquare;
+
+    Color turn;
+    std::string enPassant;
+
+    char* board;
+    bool** castleRights;
+
 
 };
 
