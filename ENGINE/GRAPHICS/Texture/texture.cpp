fd3fc2775ec71d64c220cbe57aaa61bbbdfdf539
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-21 08:45:47 +0300

    loading only rgb now

--- ENGINE/GRAPHICS/Texture/texture.cpp
+++ ENGINE/GRAPHICS/Texture/texture.cpp
@@ -6,7 +6,7 @@
     constructor
 */
 Texture2D::Texture2D()
-    : width(0), height(0), type(aiTextureType_NONE), Internal_Format(GL_RGBA), Image_Format(GL_RGBA), Wrap_S(GL_REPEAT), Wrap_T(GL_REPEAT), Filter_Min(GL_LINEAR), Filter_Max(GL_LINEAR) {
+    : width(0), height(0), type(aiTextureType_NONE), Internal_Format(GL_RGB), Image_Format(GL_RGB), Wrap_S(GL_REPEAT), Wrap_T(GL_REPEAT), Filter_Min(GL_LINEAR), Filter_Max(GL_LINEAR) {
     glGenTextures(1, &this->id);
 }
 
@@ -14,7 +14,7 @@ Texture2D::Texture2D()
 // generate texture id
 void Texture2D::generate(unsigned int width, unsigned int height, unsigned char* data) {
     this->width = width;
-    this->width = height;
+    this->height = height;
     // create Texture
     glBindTexture(GL_TEXTURE_2D, this->id);
     glTexImage2D(GL_TEXTURE_2D, 0, this->Internal_Format, width, height, 0, this->Image_Format, GL_UNSIGNED_BYTE, data);
