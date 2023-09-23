2c509827af796d8b0489e981598f2df242c5159a
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-23 20:48:42 +0300

    legal moves for pawns are written and working

--- ENGINE/ENTITY/Piece.cpp
+++ ENGINE/ENTITY/Piece.cpp
@@ -24,3 +24,27 @@ std::unordered_map<char, PieceType> Piece::pieceTypeMap = {
         {'q', PieceType::queen},
         {'k', PieceType::king}
 };
+
+std::vector<Position> Piece::getLegalMoves(const Board &board, Position currentPos) {
+    std::vector<Position> legalMoves;
+
+    if (board.getPieceAt(currentPos)->getSymbol() == 'P') {
+        if (currentPos.getRow() == 6) {
+            if (board.isEmpty(currentPos.getRow()-1, currentPos.getCol())) {
+                legalMoves.push_back(Position(currentPos.getRow() - 1, currentPos.getCol()));
+                if (board.isEmpty(currentPos.getRow()-2, currentPos.getCol())) {
+                    legalMoves.push_back(Position(currentPos.getRow() - 2, currentPos.getCol()));
+                }
+            }
+        } else {
+            if (board.isEmpty(currentPos.getRow()-1, currentPos.getCol())) {
+                legalMoves.push_back(Position(currentPos.getRow() - 1, currentPos.getCol()));
+            }
+        }
+        if (board.isEmpty(currentPos.getRow()-1, currentPos.getCol())) {
+            legalMoves.push_back(Position(currentPos.getRow() - 1, currentPos.getCol()));
+        }
+    }
+
+    return legalMoves;
+}
