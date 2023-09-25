b914734bfc94f6b0ed88fb2371e1756ad77f35e9
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-25 16:57:48 +0300

    inited and used FENparser for handling strings

--- ENGINE/ENTITY/Board.cpp
+++ ENGINE/ENTITY/Board.cpp
@@ -11,6 +11,7 @@ SpriteRenderer* Renderer;
 Board::Board(unsigned int width, unsigned int height) : keys(){
 
     moveHandler = new MoveHandler(*this);
+    fenParser = new FENparser(fenData, *this);
 
     this->height = height;
     this->width = width;
@@ -78,84 +79,10 @@ void Board::init() {
     level1.load("/Users/anhelinamodenko/CLionProjects/CHESS/Addons/media/tileData.txt", this->width, this->height);
     this->level = level1;
     this->isReversed = false;
-    parseFen(initialFEN);
+    fenParser->parseFen(initialFEN);
     rowSelected = -1;
     colSelected = -1;
 
-}
-
-void Board::parseFen(const std::string& FEN) {
-    fenData = FEN;
-
-    size_t iter = 0;
-    int index = 0;
-
-
-    // Function to set the squares on the board
-    auto set_squares = [&](char piece, int count) {
-        for (int i = 0; i < count; ++i) {
-            if (index < NB_SQ) {
-                board[index] = piece;
-                ++index;
-            }
-            else {
-                // Handle malformed FEN (buffer overflow)
-                // You may want to throw an exception or handle this differently
-                break;
-            }
-        }
-    };
-
-
-
-    // Parse the board
-    while (iter < FEN.size() && FEN[iter] != ' ') {
-        if (FEN[iter] == '/') {
-            // Handle malformed FEN (no more than 8 files in a rank)
-            // You may want to throw an exception or handle this differently
-        }
-        else if (isdigit(FEN[iter])) {
-            int count = FEN[iter] - '0';
-            set_squares('.', count);
-        }
-        else {
-            set_squares(FEN[iter], 1);
-        }
-        ++iter;
-    }
-
-
-    ++iter;
-    if (iter < FEN.size() && FEN[iter] == 'w') {
-        turn = Color::white;
-    }
-    else if (iter < FEN.size() && FEN[iter] == 'b') {
-        turn = Color::black;
-    }
-
-    iter += 2;
-    while (iter < FEN.size() && FEN[iter] != ' ') {
-        if (FEN[iter] == 'k') {
-            castleRights[static_cast<int>(Color::black)][static_cast<int>(Castle::kingSide)] = true;
-        }
-        else if (FEN[iter] == 'K') {
-            castleRights[static_cast<int>(Color::white)][static_cast<int>(Castle::kingSide)] = true;
-        }
-        else if (FEN[iter] == 'q') {
-            castleRights[static_cast<int>(Color::black)][static_cast<int>(Castle::queenSide)] = true;
-        }
-        else if (FEN[iter] == 'Q') {
-            castleRights[static_cast<int>(Color::white)][static_cast<int>(Castle::queenSide)] = true;
-        }
-        ++iter;
-    }
-
-    if (iter + 1 < FEN.size() && FEN[iter] == ' ') {
-        enPassant = FEN.substr(iter + 1, 2);
-    }
-    else {
-        enPassant = "";
-    }
 }
 
 void Board::render() {
@@ -328,7 +255,7 @@ void Board::doCollisions() {
 
                 // Check that the target square is empty or contains a piece of a different color
                 moveHandler->makeMove(selectedPiece->getPosition(), target);
-                fenData = boardToFen();
+                fenData = fenParser->boardToFen();
                 moveMade = true; // Set the flag to true
 
                 // Reset selection
@@ -354,61 +281,6 @@ void Board::renderHighlight(Position position) {
                          highlightPosition,
                          glm::vec2(widthOfSquare, heightOfSquare),
                          0.0f);
-}
-
-std::string Board::boardToFen() {
-
-    std::string fen = "";
-    int emptyCount = 0;
-
-    for (int i = 0; i < NB_SQ; i++) {
-        if (i % 8 == 0 && i != 0) {
-            if (emptyCount > 0) {
-                fen += std::to_string(emptyCount);
-                emptyCount = 0;
-            }
-            fen += '/';
-        }
-
-        if (board[i] == '.') {
-            emptyCount++;
-        } else {
-            if (emptyCount > 0) {
-                fen += std::to_string(emptyCount);
-                emptyCount = 0;
-            }
-            fen += board[i];
-        }
-    }
-    if (emptyCount > 0) {
-        fen += std::to_string(emptyCount);
-    }
-
-    fen += " ";
-    fen += turn == Color::white ? "w" : "b";
-
-    fen += " ";
-    if (castleRights[0][0] || castleRights[0][1] || castleRights[1][0] || castleRights[1][1]) {
-        if (castleRights[0][0]) fen += 'K';
-        if (castleRights[0][1]) fen += 'Q';
-        if (castleRights[1][0]) fen += 'k';
-        if (castleRights[1][1]) fen += 'q';
-    } else {
-        fen += '-';
-    }
-
-    fen += " ";
-    fen += enPassant;
-
-    fen += " ";
-    fen += "0";
-
-    fen += " ";
-    fen += "1";
-
-    return fen;
-
-
 }
 
 Board::~Board() {
