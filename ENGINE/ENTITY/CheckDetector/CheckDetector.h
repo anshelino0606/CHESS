8dde2a13bc1e4b8b3e63691b6d1dd9c5ef140a01
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-25 15:49:50 +0300

    init CheckDetector

---
+++ ENGINE/ENTITY/CheckDetector/CheckDetector.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 25.09.2023.
+//
+
+#ifndef CHESS_CHECKDETECTOR_H
+#define CHESS_CHECKDETECTOR_H
+
+
+class CheckDetector {
+
+};
+
+
+#endif //CHESS_CHECKDETECTOR_H
