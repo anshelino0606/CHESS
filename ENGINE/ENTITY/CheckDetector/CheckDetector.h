96c2184e21c35898fd49c68a426b9b89e9b0c586
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-25 17:17:53 +0300

    CheckDetector.h declared

--- ENGINE/ENTITY/CheckDetector/CheckDetector.h
+++ ENGINE/ENTITY/CheckDetector/CheckDetector.h
@@ -5,9 +5,50 @@
 #ifndef CHESS_CHECKDETECTOR_H
 #define CHESS_CHECKDETECTOR_H
 
+#include "../Board/Board.h"
 
 class CheckDetector {
+public:
 
+    /*
+     * CONSTRUCTOR
+     */
+    CheckDetector(Board& board);
+
+    /*
+     * FUNCTIONALITY
+     */
+
+    /*
+     * Checks if the current player is in check
+     * @param currentPlayerColor - color of the current player
+     */
+    bool isCheck(Color currentPlayerColor);
+
+    /*
+     * Checks if the current player is in pin
+     * @param currentPlayerColor - color of the current player
+     * @param from - position of the current player's piece
+     * @param to - position of the piece to move
+     */
+    bool isPin(Color currentPlayerColor, Position from, Position to);
+
+    /*
+     * Checks if the current player is in checkmate
+     * @param currentPlayerColor - color of the current player
+     */
+    bool isCheckmate(Color currentPlayerColor);
+
+
+
+
+private:
+
+    Board& board;
+
+    std::vector<Position> getLegalMoves(const Board& board, Position currentPos);
+    bool isMoveLegal(const Board& board, Position currentPos, Position newPos);
+    bool isKingAttacked(Color currentPlayerColor, Position kingPos);
 };
 
 
