5bc34db572d9b7b1cdef96ed748289fdedb0a83a
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-25 17:26:59 +0300

    implemented CheckDetector.cpp

--- ENGINE/ENTITY/CheckDetector/CheckDetector.cpp
+++ ENGINE/ENTITY/CheckDetector/CheckDetector.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "CheckDetector.h"
+
+std::vector<Position> CheckDetector::getLegalMoves(const Board &board, Position currentPos) {
+    std::vector<Position> legalMoves;
+    Piece* piece = board.getPieceAt(currentPos);
+    if (piece != nullptr) {
+        legalMoves = piece->getLegalMoves(board, currentPos);
+    }
+    return legalMoves;
+}
+
+bool CheckDetector::isMoveLegal(const Board &board, Position currentPos, Position newPos) {
+    std::vector<Position> legalMoves = getLegalMoves(board, currentPos);
+    for (auto move : legalMoves) {
+        if (move == newPos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool CheckDetector::isKingAttacked(Color currentPlayerColor) {
+    Position kingPos;
+    // get current player's king position
+    for (int i = 0; i < NB_SQ / 8; ++i) {
+        for (int j = 0; j < NB_SQ / 8; ++j) {
+            Piece *piece = board.getPieceAt(Position(i, j));
+            if (piece != nullptr && piece->getType() == PieceType::king && piece->getColor() == currentPlayerColor) {
+                kingPos = piece->getPosition();
+            }
+        }
+    }
+    Color opponentColor = currentPlayerColor == Color::white ? Color::black : Color::white;
+    std::vector<Position> opponentMoves;
+    // get opponent's legal moves
+    for (int i = 0; i < NB_SQ / 8; ++i) {
+        for (int j = 0; j < NB_SQ / 8; ++j) {
+            Piece *piece = board.getPieceAt(Position(i, j));
+            if (piece != nullptr && piece->getColor() == opponentColor && board.board[i*8 + j] != '.') {
+                std::vector<Position> pieceMoves = piece->getLegalMoves(board, piece->getPosition());
+                opponentMoves.insert(opponentMoves.end(), pieceMoves.begin(), pieceMoves.end());
+            }
+        }
+    }
+    for (auto move : opponentMoves) {
+        if (move == kingPos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Position CheckDetector::getOpponentKingPosition(Color currentPlayerColor) {
+    Color opponentColor = currentPlayerColor == Color::white ? Color::black : Color::white;
+    for (int i = 0; i < NB_SQ / 8; ++i) {
+        for (int j = 0; j < NB_SQ / 8; ++j) {
+            Piece *piece = board.getPieceAt(Position(i, j));
+            if (piece != nullptr && piece->getType() == PieceType::king && piece->getColor() == opponentColor) {
+                return piece->getPosition();
+            }
+        }
+    }
+}
