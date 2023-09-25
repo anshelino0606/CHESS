b62529e155ac41459fec602638bc0101231e9ad4
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-25 15:55:08 +0300

    MoveHandler functions declared

--- ENGINE/ENTITY/MoveHandler/MoveHandler.h
+++ ENGINE/ENTITY/MoveHandler/MoveHandler.h
@@ -5,9 +5,49 @@
 #ifndef CHESS_MOVEHANDLER_H
 #define CHESS_MOVEHANDLER_H
 
+#include "../Board.h"
 
 class MoveHandler {
 
+public:
+
+    /*
+     * CONSTRUCTOR
+     */
+
+    MoveHandler(Board& board);
+
+    /*
+     * FUNCTIONS
+     */
+
+    /*
+     * Moves a piece from one cell to another
+     * @param from - position of the piece to move
+     * @param to - position of the cell to move to
+     * @explicit param board - board to move on
+     */
+    bool makeMove(Position from, Position to);
+
+    /*
+     * Checks if the cell is empty
+     * in a char* array of 64 elements
+     * @param row - row of the cell
+     * @param col - column of the cell
+     */
+    bool isEmpty(int row, int col);
+
+    /*
+     * Checks if the cell has Piece of opposite color
+     * @param row - row of the cell
+     * @param col - column of the cell
+     * @param currentPlayerColor - color of the current player
+     */
+    bool isOppositeColor(int row, int col, Color currentPlayerColor) const;
+
+
+private:
+    Board& board;
 };
 
 
