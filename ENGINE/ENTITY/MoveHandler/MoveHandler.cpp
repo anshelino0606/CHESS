1e1281f5eae23edb809a18599610213654251930
Anhelina Modenko <anhelinayolo04@icloud.com>
2023-09-25 15:49:30 +0300

    init MoveHandler

---
+++ ENGINE/ENTITY/MoveHandler/MoveHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anhelina Modenko on 25.09.2023.
+//
+
+#include "MoveHandler.h"
---
+++ ENGINE/ENTITY/MoveHandler/MoveHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by Anhelina Modenko on 25.09.2023.
+//
+
+#ifndef CHESS_MOVEHANDLER_H
+#define CHESS_MOVEHANDLER_H
+
+
+class MoveHandler {
+
+};
+
+
+#endif //CHESS_MOVEHANDLER_H
